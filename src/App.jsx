e9bc064b043eaa0cf532a5ca7f7b3aa9142831e1
JaesungKo <jaesungko.dev@gmail.com>
2025-04-28 23:14:58 +0900

    React-Router-Dom을 사용하여 라우팅 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-// import movieList from "@/mocks/movieListData.json";
+import { Routes, Route } from "react-router-dom";
+import movieList from "@/mocks/movieListData.json";
 import movieDetail from "@/mocks/movieDetailData.json";
-// import MovieCard from "@/components/MovieCard";
-import MovieDetail from "./components/MovieDetail";
+import MovieCard from "@/components/MovieCard";
+import MovieDetail from "@/components/MovieDetail";
 
 const App = () => {
   return (
     <>
-      {/* <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mx-5 mt-5">
-        {movieList.results.map((movie) => (
-          <MovieCard key={movie.id} data={movie} />
-        ))}
-      </div> */}
-      <MovieDetail movieDetail={movieDetail} />
+      <Routes>
+        <Route path="/" element={<MovieCard data={movieList.results} />} />
+        <Route
+          path="/details"
+          element={<MovieDetail movieDetail={movieDetail} />}
+        />
+      </Routes>
     </>
   );
 };
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 const MovieCard = ({ data }) => {
   return (
-    <div className="border p-4 rounded-lg shadow-lg">
-      <img
-        src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
-        alt={data.title}
-        className="w-full h-80 object-cover"
-      />
-      <h2 className="mt-2 font-bold">{data.title}</h2>
-      <p className="text-sm text-right">평점: {data.vote_average}</p>
+    <div className="m-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {data.map((movie) => (
+        <div className="border p-4 rounded-lg shadow-lg" key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            className="w-full h-100 object-cover"
+          />
+          <h2 className="mt-2 font-bold">{movie.title}</h2>
+          <p className="text-sm text-right">평점: {movie.vote_average}</p>
+        </div>
+      ))}
     </div>
   );
 };
