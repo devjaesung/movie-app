835a6c625f296af282255520a08b3a6e03f51f92
JaesungKo <jaesungko.dev@gmail.com>
2025-04-28 22:53:42 +0900

    더미데이터를 사용한 상세 페이지 (MovieDetail.jsx) 레이아웃 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import data from "@/mocks/movieListData.json";
-import MovieCard from "@/components/MovieCard";
+// import movieList from "@/mocks/movieListData.json";
+import movieDetail from "@/mocks/movieDetailData.json";
+// import MovieCard from "@/components/MovieCard";
+import MovieDetail from "./components/MovieDetail";
 
 const App = () => {
   return (
     <>
-      <div className="my-5 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mx-5 mt-5">
-        {data.results.map((movie) => (
+      {/* <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mx-5 mt-5">
+        {movieList.results.map((movie) => (
           <MovieCard key={movie.id} data={movie} />
         ))}
-      </div>
+      </div> */}
+      <MovieDetail movieDetail={movieDetail} />
     </>
   );
 };
---
+++ src/components/MovieDetail.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const MovieDetail = ({ movieDetail }) => {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="min-w-100 w-full max-w-1/2 h-screen">
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`}
+          alt={movieDetail.title}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="flex-1">
+        <div className="flex justify-between items-center p-4 bg-gray-800 text-white mb-5">
+          <h1 className="text-2xl font-bold">{movieDetail.title}</h1>
+          <p>평점: {movieDetail.vote_average}</p>
+        </div>
+        <div>
+          <div className="px-2">
+            <p className="text-right text-sm text-gray-500 mb-6">
+              장르: {movieDetail.genres?.map((genre) => genre.name).join(", ")}
+            </p>
+            <p>{movieDetail.overview ?? "정보 없음"}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetail;
